4a8f508b5a7e840c837a046f970e3b6f6f7a69f6
Jakub Niewadzi <kubanie2002@gmail.com>
2018-11-28 21:47:30 +0100

    Initial commit

--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,10 +1,13 @@
 package com.company;
 
 public class Student {
-    String name = "Jakub";
-    String surename = "Niewadzi";
-    int age = 16;
-    public Student(){
+    String name;
+    String surename;
+
+
+    public Student(String name, String surename){
+        this.name = name;
+        this.surename = surename;
 
     }
 }
--- src/com/company/StudentRepository.java
+++ src/com/company/StudentRepository.java
@@ -1,4 +1,24 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
 public class StudentRepository {
+    Student student = new Student("Jakub", "Niewadzi");
+    ArrayList list = new ArrayList();
+
+    public void WriteListObject() {
+        list.add(student.name);
+        list.add(student.surename);
+        Iterator itr = list.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+
+        }
+
+    }
+
+
 }
+
